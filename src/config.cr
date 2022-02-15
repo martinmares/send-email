f2ecb40d5a89e86c04badb11ee430afd795df94b
Martin Mareš <mares@datalite.cz>
2022-02-15 19:00:04 +0100

    Add support for config

---
+++ src/config.cr
@@ -0,0 +1,58 @@
+require "yaml"
+
+module SendEmail
+  class Config
+    include YAML::Serializable
+
+    @[YAML::Field(key: "smtp")]
+    property smtp : Smtp
+
+    @[YAML::Field(key: "mail")]
+    property mail : Mail
+
+    def self.load(from = file)
+      content = File.read(from)
+      self.from_yaml(content)
+    end
+  end
+
+  class Smtp
+    include YAML::Serializable
+
+    @[YAML::Field(key: "hostname")]
+    property hostname : String
+
+    @[YAML::Field(key: "port")]
+    property port : Int32
+  end
+
+  class Mail
+    include YAML::Serializable
+
+    @[YAML::Field(key: "subject")]
+    property subject : String
+
+    @[YAML::Field(key: "from")]
+    property from : Email
+
+    @[YAML::Field(key: "to", emit_null: true)]
+    property to : Array(Email)?
+
+    @[YAML::Field(key: "cc", emit_null: true)]
+    property cc : Array(Email)?
+
+    @[YAML::Field(key: "bcc", emit_null: true)]
+    property bcc : Array(Email)?
+end
+
+  class Email
+    include YAML::Serializable
+
+    @[YAML::Field(key: "address")]
+    property address : String
+
+    @[YAML::Field(key: "name", emit_null: true)]
+    property from : String?
+  end
+
+end
--- src/send-email.cr
+++ src/send-email.cr
@@ -1,28 +1,31 @@
 require "email"
 require "option_parser"
 
+require "../src/arg_parser"
+require "../src/config"
+
 module SendEmail
   VERSION = "0.1.0"
 
   class App
-    getter :hostname, :port
+    getter :config
 
-    @hostname : String
-    @port : Int32
-    @debug : Bool
+    @config : Config
+    @arg_pars : ArgParser
 
-    @subject : String
-    @text : String
-    @html : String
-
-    @from : String
-    @recipients : Array(String)
+    def initialize()
+      @arg_pars = ArgParser.new
+      @arg_pars.parse
+      @arg_pars.check
+      @config = Config.load from: @arg_pars.args[:config]
+      p @config
+    end
 
     def run()
         # Create email message
         email = EMail::Message.new
         email.from    "noreply@datalite.cz", "Notify"
-        email.to      "mares@datalite.cz"
+        email.to      "mares@datalite.cz", "Martin Mareš"
         email.subject "Test email - hello world!"
         email.message <<-EOM
           Testovací email.
@@ -36,10 +39,6 @@ module SendEmail
 
         EOM
 
-        # Set SMTP client configuration
-        # config = EMail::Client::Config.new("smtp.datalite.cz", 587)
-        # config.use_tls(EMail::Client::TLSMode::STARTTLS)
-        # config.tls_context.verify_mode = OpenSSL::SSL::VerifyMode::NONE
         config = EMail::Client::Config.new("oracleas.datalite.cz", 2525)
 
         # Create SMTP client object
@@ -51,57 +50,9 @@ module SendEmail
         end
     end
 
-    def initialize()
-      @hostname = ""
-      @port = -1
-      @debug = false
-
-      @subject = ""
-      @text = ""
-      @html = ""
-
-      @from = ""
-      @recipients = [] of String
-
-      _opts = parse_opts()
-    end
-
-    def parse_opts
-      OptionParser.parse do |parser|
-        parser.banner = "Usage: send-email [arguments]"
-
-        parser.on("-h HOSTNAME", "--hostname=HOSTNAME", "Hostname (SMTP)") { |_hostname| @hostname = _hostname }
-        parser.on("-p PORT", "--port=PORT", "Port") { |_port| @port = _port.to_i }
-        parser.on("-s SUBJECT", "--subject=SUBJECT", "Subject") { |_subject| @subject = _subject }
-        parser.on("-f FROM", "--from=FROM", "From:") { |_from| @from = _from }
-        parser.on("-r RECIPIENTS", "--recipients=RECIPIENTS", "RcptTo: (comma separated list)") do |_recipients|
-          @recipients = _recipients.split(',')
-        end
-        parser.on("-t TXT", "--txt-body-file=TXT", "Text body file") { |_text| @text = _text }
-        parser.on("-m HTML", "--html-body-file=HTML", "Html body file") { |_html| @html = _html }
-
-        parser.on("-d", "--debug", "Debug?") { @debug = true }
-        parser.on("-v", "--version", "App version") do
-          puts "App name: send-email"
-          puts "App version: #{VERSION}"
-          exit
-        end
-        parser.on("-h", "--help", "Show this help") do
-          puts parser
-          exit
-        end
-        parser.invalid_option do |flag|
-          STDERR.puts "ERROR: #{flag} is not a valid option."
-          STDERR.puts parser
-          exit(1)
-        end
-
-      end
-    end
-
   end
 
   app = App.new
-  app.run
+  # app.run
 
 end
